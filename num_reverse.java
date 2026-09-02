a9e1933ea92b04182072a66db5a230c81d5a0e7e
TR KARTHIKEYA <karthikeyatr7@gmail.com>
2026-09-02 20:51:35 +0530

    Add files via upload

---
+++ num_reverse.java
@@ -0,0 +1,18 @@
+package Day_06;
+import java.util.Scanner;
+public class num_reverse{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int rem;
+        int rev = 0;
+        while(num != 0){
+            rem = num % 10;
+            rev = (rev*10) + rem;
+            num = num/10;
+        }
+        System.out.println("Rev = " + rev);
+        sc.close();
+    }
+}
